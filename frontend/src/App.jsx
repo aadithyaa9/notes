ee0c0b800bb28d05195e83bb94dc991666a67069
fujitsu11 <aadiramki01@gmail.com>
2025-07-14 01:35:23 +0530

    Remove Test lines

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,14 +6,7 @@ import NoteDetail from './pages/NoteDetail';
 import toast from 'react-hot-toast';
 const App = () => {
   return( <div data-theme="coffee">
-      <button className="btn btn-neutral">Neutral</button>
-<button className="btn btn-primary">Primary</button>
-<button className="btn btn-secondary">Secondary</button>
-<button className="btn btn-accent">Accent</button>
-<button className="btn btn-info">Info</button>
-<button className="btn btn-success">Success</button>
-<button className="btn btn-warning">Warning</button>
-<button className="btn btn-error">Error</button>
+    
       <Routes>
         <Route path="/" element={<Homepage/>} />
         <Route path="/create" element={<Create/>} />
